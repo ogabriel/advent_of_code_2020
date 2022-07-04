419955fa5bf489bacee40222ee4da5e538d6d5cc
ogabriel <gabrieloliver8991@gmail.com>
2022-07-04 14:16:46 -0300

    add 1 ruby solution

---
+++ 1/ruby/main.rb
@@ -0,0 +1,30 @@
+nums = File.readlines("#{__dir__}/../input.txt")
+
+TARGET = 2020
+
+nums.map! do |num|
+  num.to_i
+end
+
+nums.sort!
+
+SIZE = nums.size / 2
+HALF_SIZE = nums.size / 2
+
+num1 = 0
+num2 = 0
+
+nums.each do |temp_num1|
+  nums.reverse_each do |temp_num2|
+    if temp_num1 + temp_num2 == 2020
+      num1 = temp_num1
+      num2 = temp_num2
+
+      break
+    end
+  end
+end
+
+product = num1 * num2
+
+puts "the product of both nums is #{product}"
